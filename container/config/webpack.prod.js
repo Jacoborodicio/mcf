68b1c0aa3766d9ab5e37b2937666be5061b47999
Jacobo <jacoborodicio@gmail.com>
2021-11-13 01:11:38 +0100

    chore: mounting remote school of code module

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -17,7 +17,7 @@ const prodConfig = {
             remotes: {
                 marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
                 dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
-                codeSchool: `codeSchool@${domain}/codeSchool/latest/remoteEntry.js`,
+                codeSchool: `code_school@${domain}/code-school/latest/remoteEntry.js`,
             },
             shared: packageJson.dependencies,
         })
